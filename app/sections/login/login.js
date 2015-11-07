63fc052f6e3a7a77dfb8c46ea57dd8cae8e42072
Chris Gosselin <chrisgosselin@Christophers-MacBook-Pro-2.local>
2015-11-07 10:57:12 -0500

    enter to login works

--- app/sections/login/login.js
+++ app/sections/login/login.js
@@ -4,11 +4,15 @@ angular.module('ianmd.sections.login', ['ianmd.core.user.service'])
 
 .controller('LoginCtrl', function(userService, $location) {
   (function (vm) {
+    vm.msg = '';
+
     vm.login = function vmLogin(){
       var success = userService.login(vm.username, vm.password);
 
       if (success) {
         $location.path('/home');
+      } else {
+        vm.msg = 'Credentials are not correct';
       }
     };
   })(this);
